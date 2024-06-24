30e02025eed11279ecfac89b20716563beea37b6
Marcus Radisch <radischm@googlemail.com>
2024-06-24 20:34:55 +0200

    Aktualisieren von connection_ping.sh
    "$? fragt den Exit des letzten Anfrage an" bei ping -c 10 wird logischerweise nur der 10. abgefragt"

--- connection_ping.sh
+++ connection_ping.sh
@@ -3,7 +3,7 @@ touch $2
 while true;
 do
    DATE=$(date '+%d/%m/%Y %H:%M:%S')
-   ping -c 10 $1 &> /dev/null
+   ping -c 1 $1 &> /dev/null
    if [[ $? -ne 0 ]]; then
      echo "ERROR $1 is not reachable at" $DATE >> $2
    else
